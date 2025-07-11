c0107c84fb1163e8db440900c31a3b64f5e48f4c
callum <callumery11@outlook.com>
2025-07-11 09:23:01 +0100

    Easier to test

--- src/main/java/io/AdiK5050/advCalculator/inputhandling/interfaces/ExpressionChoiceInput.java
+++ src/main/java/io/AdiK5050/advCalculator/inputhandling/interfaces/ExpressionChoiceInput.java
@@ -3,5 +3,5 @@ package main.java.io.AdiK5050.advCalculator.inputhandling.interfaces;
 import java.util.Scanner;
 
 public interface ExpressionChoiceInput {
-    int getUserInput(Scanner sc);
+    int getUserInput();
 }
